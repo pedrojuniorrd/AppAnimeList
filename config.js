c7c4a41dd6fe22114505c43770aacf1ce8124819
Pedro <52047256+pedrojuniorrd@users.noreply.github.com>
2021-04-19 11:37:11 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
-// Initialize Firebase
+// credenciais do firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBmMKQH5lbziClzA6FpGhL-pHZQRn53Zyo",
-    authDomain: "animelist-a4ee1.firebaseapp.com",
-    databaseURL: "https://animelist-a4ee1.firebaseio.com",
-    projectId: "animelist-a4ee1",
-    storageBucket: "animelist-a4ee1.appspot.com",
-    messagingSenderId: "868752723753",
-    appId: "1:868752723753:web:1f66fd4ff9cfaf3d183138",
+    apiKey: 
+    authDomain:
+    databaseURL:
+    projectId: 
+    storageBucket: 
+    messagingSenderId: 
+    appId: 
 
 
     };
